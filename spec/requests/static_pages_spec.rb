b79cbc051491e61225aec35789792797fc4fe4af
slyelza <a.tiptsova@gmail.com>
2016-05-10 00:16:12 +0300

    postresql

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,11 +2,13 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Аrielza" }
+
   describe "Home page" do
 
     it "should have the right title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
 
     it "should have the content 'Аrielza App'" do
@@ -19,7 +21,7 @@ describe "Static pages" do
 
     it "should have the right title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
 
     it "should have the content 'Help'" do
@@ -32,12 +34,25 @@ describe "Static pages" do
 
     it "should have the right title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
 
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the right title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
     end
   end
 end
